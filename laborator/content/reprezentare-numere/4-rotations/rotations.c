7d4a3822e5a138715fdbef2dee00d45f3e34c67c
GeorgeRaducanu <georgecristian2002@gmail.com>
2022-03-06 14:29:44 +0200

    lab1 : ex4: Solution of rotation using bitwise operations. The left and right rotations are implemented oneliner style. For a better view it's best to work with unsgned. This program uses signed as this was the model.
    Signed-off-by: GeorgeRaducanu <georgecristian2002@gmail.com>

--- laborator/content/reprezentare-numere/4-rotations/rotations.c
+++ laborator/content/reprezentare-numere/4-rotations/rotations.c
@@ -3,21 +3,24 @@
 void rotate_left(int *number, int bits)
 {
 	/* TODO */
-	(void) number;
-	(void) bits;
+	*number = (*number << bits) | ((*number) >> (32 - bits));
 }
 
 void rotate_right(int *number, int bits)
 {
 	/* TODO */
-	(void) number;
-	(void) bits;
+	*number = (*number >> bits) | ((*number) << (32 - bits));
 }
 
 int main()
 {
 	/* TODO: Test functions */
-
+	int num, bits;
+	scanf("%d %d", &num, &bits);
+	int clone = num;
+	rotate_left( &num, bits);
+	printf("rotate_left %d\n", num);
+	rotate_right(&clone, bits);
+	printf("rotate_right %d\n", clone);
 	return 0;
 }
-
