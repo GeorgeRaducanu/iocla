be15d4f4d74e404218792919f1bbddda0e9c1241
GeorgeRaducanu <georgecristian2002@gmail.com>
2022-03-06 14:26:35 +0200

    lab1: ex3 : Solution of reversing a string using pointers and my_strlen function
    Signed-off-by: GeorgeRaducanu <georgecristian2002@gmail.com>

--- laborator/content/reprezentare-numere/3-mirror/mirror.c
+++ laborator/content/reprezentare-numere/3-mirror/mirror.c
@@ -2,16 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+int my_strlen(const char *str)
+{
+	/* TODO */
+	int count = 0;
+	while (*(str + count)) {
+		count++;
+	}
+	return count;
+}
+
 void mirror(char *s)
 {
 	/* TODO */
-	(void) s;
+	int N = my_strlen(s);
+	int i = 0;
+	char a;
+	while(i< (N>>1)) {
+		a = *(s+i);
+		*(s+i) = *(s + (N - i -1));
+		*(s + (N - i -1)) = a;
+		i++;
+	}
+	printf("%s\n", s);
 }
 
 int main()
 {
 	/* TODO: Test function */
-
+	char s[100];
+	scanf("%s", s);
+	mirror(s);
 	return 0;
 }
-
