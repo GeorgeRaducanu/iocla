7f29f099ec3f9aa6f81b68dc8e25832a599439c9
GeorgeRaducanu <georgecristian2002@gmail.com>
2022-03-06 14:19:35 +0200

    lab1 : ex2 : Add solution for ex2
    Implementation of strlen using pointers
    
    Signed-off-by: GeorgeRaducanu <georgecristian2002@gmail.com>

--- laborator/content/reprezentare-numere/2-len_xor/len_xor.c
+++ laborator/content/reprezentare-numere/2-len_xor/len_xor.c
@@ -5,21 +5,31 @@
 int my_strlen(const char *str)
 {
 	/* TODO */
-	(void) str;
-
-	return -1;
+	int count = 0;
+	while (*(str + count)) {
+		count++;
+	}
+	return count;
 }
 
 void equality_check(const char *str)
 {
 	/* TODO */
-	(void) str;
+	int dim=my_strlen(str);
+	int i = 0;
+	while (i < dim) {
+		if (!(*(str + i))^((*(str + i + (1<<i))%dim)))
+			printf("Address of %c is %p and %p\n", *(str+i), str+i, str+i+(1<<i));
+		i++;
+	}
 }
 
 int main(void)
 {
 	/* TODO: Test functions */
-
+	char s[100];
+	scanf("%s", s);
+	printf("%d\n", my_strlen(s) );
+	equality_check(s);
 	return 0;
 }
-
