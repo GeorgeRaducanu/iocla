b2f6eb891489f45a98f424ed4645cca7f78611b1
GeorgeRaducanu <georgecristian2002@gmail.com>
2022-03-06 14:35:59 +0200

    lab1:ex5: Elementary solution for representating numbers in base 16 and 2 using bitwise operations
    Signed-off-by: GeorgeRaducanu <georgecristian2002@gmail.com>

--- laborator/content/reprezentare-numere/5-odd_even/odd_even.c
+++ laborator/content/reprezentare-numere/5-odd_even/odd_even.c
@@ -1,24 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int my_strlen(int *str)
+{
+	/* TODO */
+	int count = 0;
+	while (*(str + count)) {
+		count++;
+	}
+	return count/2;
+}
+
 void print_binary(int number, int nr_bits)
 {
 	/* TODO */
-	(void) number;
-	(void) nr_bits;
+	nr_bits = 32;
+	printf("0b0");
+	long i;
+	for (i = (1<<(nr_bits -2)); i > 0; i=i/2) {
+		if(number&i)
+			printf("1");
+		else
+			printf("0");
+	}
+	printf("\n");
 }
 
 void check_parity(int *numbers, int n)
 {
 	/* TODO */
-	(void) numbers;
-	(void) n;
+	int nr = 32;
+	//n=my_strlen(*numbers);
+	for (int i = 0; i < n; ++i)
+		if( (*(numbers + i) | 1) > (*(numbers + i)))
+			print_binary(*(numbers +i), nr);
+		else {
+			printf("0x%x\n", (*(numbers + i)));
+		}
 }
 
 int main()
 {
 	/* TODO: Test functions */
-
+	int numbers[3]={214,233271,84};
+	int n=my_strlen(numbers);
+	check_parity(numbers, n);
 	return 0;
 }
 
