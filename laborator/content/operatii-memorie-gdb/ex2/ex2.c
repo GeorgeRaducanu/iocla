89acf7aee900f91af6189f09d243a11d613afa13
George Raducanu <georgecristian2002@gmail.com>
2022-03-13 13:21:33 +0200

    lab2:ex2 Deletion first appearence of pattern from s using strstr
    Signed-off-by: George Raducanu <georgecristian2002@gmail.com>

--- laborator/content/operatii-memorie-gdb/ex2/ex2.c
+++ laborator/content/operatii-memorie-gdb/ex2/ex2.c
@@ -2,14 +2,30 @@
 #include <string.h>
 #include <stdlib.h>
 
-char* delete_first(char *s, char *pattern);
+char* delete_first(char *s, char *pattern)
+{
+	// char *neww = (char *)malloc((strlen(s) - strlen(pattern))*sizeof(char));
+	// char *p = strstr(s, pattern);
+	// if(p) {
+	// 	for (int i = 0; i < (int)(p - s); i++)
+	// 		*(neww + i) = *(s + i);
+	// 	for (int i = (int)(p - s); i < strlen(s); ++i)
+	// 		*(neww + i) = *(s + i - strlen(pattern));
+	// }
+	// return (char *)neww;
+	char *aux = strstr(s, pattern);
+	if (aux != NULL) {
+		strcpy(aux, aux+strlen(pattern));
+	}
+	return aux;
+}
 
 int main(){
 	char *s = "Ana are mere";
 	char *pattern = "re";
-
+	char *p;
 	// Decomentati linia dupa ce ati implementat functia delete_first.
-	// printf("%s\n", delete_first(s, pattern));
-
+	printf("%s\n", delete_first(p = strdup(s), pattern));
+	free(p);
 	return 0;
 }
