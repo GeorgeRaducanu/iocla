3782fe5069cc42c77df0576b841d1093f2e42554
George Raducanu <georgecristian2002@gmail.com>
2022-03-25 20:15:31 +0200

    lab4:ex3 Binary search using goto
    Signed-off-by: George Raducanu <georgecristian2002@gmail.com>

--- laborator/content/toolchain-decompilare/3-goto-algs/binary_search.c
+++ laborator/content/toolchain-decompilare/3-goto-algs/binary_search.c
@@ -3,12 +3,25 @@
 int main(void)
 {
 	int v[] =  {1, 2, 15, 51, 53, 66, 202, 7000};
-	int dest = v[2]; /* 15 */
+	int dest = v[5]; /* 15 */
 	int start = 0;
 	int end = sizeof(v) / sizeof(int) - 1;
 
 	/* TODO: Implement binary search */
-	(void) dest;
-	(void) start;
-	(void) end;
+label:
+	if (v[(start+end)/2] == dest)
+		goto out;
+	if (v[(start+end)/2] < dest) {
+		start = (start+end)/2;
+		goto label;
+	}
+	if (v[(start+end)/2] > dest) {
+		end = (start+end)/2;
+		goto label;
+	}
+out:
+	// (void) dest;
+	// (void) start;
+	// (void) end;
+	printf("%d\n", (start+end)/2);
 }
