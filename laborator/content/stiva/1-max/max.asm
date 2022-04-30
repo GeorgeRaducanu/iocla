903e4002d8cc8da72951677d7cbbd7f8a3ebc48d
George Raducanu <georgecristian2002@gmail.com>
2022-04-30 13:37:20 +0300

    lab8 ex1 Solved
    Signed-off-by: George Raducanu <georgecristian2002@gmail.com>

--- laborator/content/stiva/1-max/max.asm
+++ laborator/content/stiva/1-max/max.asm
@@ -10,7 +10,12 @@ main:
     mov ebx, 4 
 
     ; TODO: get maximum value. You are only allowed to use one conditional jump and push/pop instructions.
-
+    cmp eax, ebx
+    jg end
+    push eax
+    push ebx
+    pop eax
+    pop ebx
     PRINTF32 `Max value is: %d\n\x0`, eax ; print maximum value
-
+end:
     ret
