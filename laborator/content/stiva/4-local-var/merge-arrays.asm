ce511dadd7c58a151529a25587a78905a7cced0c
George Raducanu <georgecristian2002@gmail.com>
2022-04-30 13:49:51 +0300

    lab8 ex4 attempt to solve
    Signed-off-by: George Raducanu <georgecristian2002@gmail.com>

--- laborator/content/stiva/4-local-var/merge-arrays.asm
+++ laborator/content/stiva/4-local-var/merge-arrays.asm
@@ -19,56 +19,80 @@ main:
     mov eax, 0 ; counter used for array_1
     mov ebx, 0 ; counter used for array_2
     mov ecx, 0 ; counter used for the output array
-
+    push ebp
+    mov ebp, esp
+    sub esp, ARRAY_OUTPUT_LEN * 4
 merge_arrays:
+    xor edx, edx
     mov dl, byte [array_1 + eax]
     mov dh, byte [array_2 + ebx]
     cmp dl, dh
     jg array_2_lower
+
 array_1_lower:
-    mov byte [array_output + ecx], dl
-    inc eax
+    ;mov byte [array_output + ecx], dl
+    ;inc eax
+    mov dword[esp + ecx * 4], 0
+    mov byte[esp + ecx * 4], dl
     inc ecx
+    inc eax
     jmp verify_array_end
+
 array_2_lower:
-    mov byte [array_output + ecx], dh
+    ;mov byte [array_output + ecx], dh
+    mov dword[esp + ecx * 4], 0
+    mov byte[esp + ecx * 4], dh
     inc ecx
     inc ebx
+    jmp verify_array_end
 
 verify_array_end:
     cmp eax, ARRAY_1_LEN
-    jge copy_array_2
+    je copy_array_2
     cmp ebx, ARRAY_2_LEN
-    jge copy_array_1
+    je copy_array_1
     jmp merge_arrays
 
 copy_array_1:
+    cmp eax, ARRAY_1_LEN
+    je print_array
+    xor edx, edx
     mov dl, byte [array_1 + eax]
-    mov byte [array_output + ecx], dl
+    ;mov byte [array_output + ecx], dl
+    mov dword[esp + ecx * 4], 0
+    mov byte[esp + ecx * 4], dl
     inc ecx
     inc eax
-    cmp eax, ARRAY_1_LEN
-    jb copy_array_1
-    jmp print_array
+    jmp copy_array_1
+
 copy_array_2:
-    mov dh, byte [array_2 + ebx]
-    mov byte [array_output + ecx], dh
+    cmp ebx, ARRAY_2_LEN
+    ;mov dh, byte [array_2 + ebx]
+    ;mov byte [array_output + ecx], dh
+    mov dword[esp + ecx * 4], 0
+    mov byte[esp + ecx * 4], dl
     inc ecx
     inc ebx
-    cmp ebx, ARRAY_2_LEN
-    jb copy_array_2
+    jmp copy_array_2
 
 print_array:
     PRINTF32 `Array merged:\n\x0`
     mov ecx, 0
+    
 print:
-    mov al, byte [array_output + ecx]
+    cmp ecx, ARRAY_OUTPUT_LEN
+    je final
+    xor eax, eax
+    mov eax, dword[esp + ecx * 4]
     PRINTF32 `%d \x0`, eax
     inc ecx
-    cmp ecx, ARRAY_OUTPUT_LEN
-    jb print
+    jmp print
+final:
+    add esp, ARRAY_OUTPUT_LEN * 4
+    mov esp, ebp
+    pop esp
+    xor eax, eax
 
     PRINTF32 `\n\x0`
-    xor eax, eax
     ret
     
